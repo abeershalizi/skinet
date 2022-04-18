82902128956fc2b2ef98c84c062ead37c4717060
unknown <16050141001@ybu.edu.tr>
2022-04-18 12:48:13 +0200

    Section 2

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,24 @@
+namespace API;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string Summary { get; internal set; }
+
+    private string summary;
+
+    public string GetSummary()
+    {
+        return summary;
+    }
+
+    public void SetSummary(string value)
+    {
+        summary = value;
+    }
+}
